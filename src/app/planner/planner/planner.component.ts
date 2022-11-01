38aebacd038801158b7f6a9d4bdfa92eae405c30
Mark Bolier <markbolier@icloud.com>
2022-11-01 15:00:16 +0100

    bunch of styling

--- src/app/planner/planner/planner.component.ts
+++ src/app/planner/planner/planner.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlannerComponent implements OnInit {
 
+meals = [
+    { id: 1, name: 'Incredible Sicilian aubergine stew (Caponata)', size: '2p', img: 'https://img.jamieoliver.com/jamieoliver/recipe-database/oldImages/large/280_1_1438788317.jpg?tr=w-650,h-866' },
+    { id: 2, name: 'Incredible Sicilian aubergine stew (Caponata)', size: '2p', img: 'https://img.jamieoliver.com/jamieoliver/recipe-database/oldImages/large/280_1_1438788317.jpg?tr=w-650,h-866' },
+    { id: 3, name: 'Incredible Sicilian aubergine stew (Caponata)', size: '2p', img: 'https://img.jamieoliver.com/jamieoliver/recipe-database/oldImages/large/280_1_1438788317.jpg?tr=w-650,h-866' },
+    { id: 4, name: 'Incredible Sicilian aubergine stew (Caponata)', size: '2p', img: 'https://img.jamieoliver.com/jamieoliver/recipe-database/oldImages/large/280_1_1438788317.jpg?tr=w-650,h-866' },
+    { id: 5, name: 'Incredible Sicilian aubergine stew (Caponata)', size: '2p', img: 'https://img.jamieoliver.com/jamieoliver/recipe-database/oldImages/large/280_1_1438788317.jpg?tr=w-650,h-866' },
+];
+  
   constructor() { }
 
   ngOnInit(): void {
