8f468e7fc6e0cc6d0df9405826f3f961bef92bde
Mark Bolier <markbolier@icloud.com>
2022-11-02 11:28:17 +0100

    Added button component and added add button to planner component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    NavigationComponent
+    NavigationComponent,
+    ButtonComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    NavigationComponent
+    NavigationComponent,
+    ButtonComponent
   ]
 })
 export class SharedModule { }
