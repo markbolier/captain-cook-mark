69537cd1fec1c9ef9e5549a43f1462309c8a17c9
Mark Bolier <markbolier@icloud.com>
2022-11-02 10:20:13 +0100

    added card component in planner and passed down props

--- src/app/planner/planner.module.ts
+++ src/app/planner/planner.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { PlannerComponent } from './planner/planner.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    PlannerComponent
+    PlannerComponent,
+    CardComponent
   ],
   imports: [
     CommonModule,
