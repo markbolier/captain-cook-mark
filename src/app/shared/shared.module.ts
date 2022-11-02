d948ce37b5c18b021ec5e514720f0fd012dc6ad9
Mark Bolier <markbolier@icloud.com>
2022-11-02 15:44:57 +0100

    Added dropdown component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
 
 @NgModule({
   declarations: [
-    NavigationComponent
+    NavigationComponent,
+    DropdownComponent
   ],
   imports: [
     CommonModule
