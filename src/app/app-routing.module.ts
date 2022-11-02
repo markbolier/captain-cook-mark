12e841ef1ac16590bd3b29763e56995aa2bd4322
Mark Bolier <markbolier@icloud.com>
2022-11-02 15:26:24 +0100

    Added routing for recipes and planner

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlannerComponent } from './planner/planner/planner.component';
+import { RecipesComponent } from './recipes/recipes/recipes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'recipes', component: RecipesComponent },
+  {path: '', component: PlannerComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
