d3de645bd87e2c400099c89e015649647d519e88
Mark Bolier <markbolier@icloud.com>
2022-11-03 12:03:02 +0100

    Added planner routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,10 @@ import { PlannerComponent } from './planner/planner/planner.component';
 import { RecipesComponent } from './recipes/recipes/recipes.component';
 
 const routes: Routes = [
-  { path: 'recipes', component: RecipesComponent },
   { path: '', component: PlannerComponent },
   { path: '**', component: PageNotFoundComponent },
+  { path: 'planner', component: PlannerComponent },
+  { path: 'recipes', component: RecipesComponent },
 ];
 
 @NgModule({
