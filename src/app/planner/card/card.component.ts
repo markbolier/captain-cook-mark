dff95f5184bb155339307e21967853d5e7626fea
Mark Bolier <markbolier@icloud.com>
2022-11-07 09:47:05 +0100

    Some styling

--- src/app/planner/card/card.component.ts
+++ src/app/planner/card/card.component.ts
@@ -1,27 +1,26 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  selector: "app-card",
+  templateUrl: "./card.component.html",
+  styleUrls: ["./card.component.scss"],
 })
 export class CardComponent implements OnInit {
-  @Input() id: any;
-  @Input() size: any;
-  @Input() name: any;
-  @Input() img: any;
-  @Input() recipes: any;
-  @Input() i: any;
+  @Input() id!: number;
+
+  @Input() size!: string;
+
+  @Input() name!: string;
+
+  @Input() img!: string;
 
   @Output() delete = new EventEmitter<number>();
 
   onClick(id: number) {
-  this.delete.emit(id);
-  }
-  
-  constructor() { }
-
-  ngOnInit(): void {
+    this.delete.emit(id);
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
